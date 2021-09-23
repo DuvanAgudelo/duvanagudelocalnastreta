a1f1a1747f689a730b7b81597df3579cafda0de3
Duvan Agudelo <daagudeloa@unal.edu.co>
2021-09-23 15:34:15 -0500

    sin mas branches

---
+++ scr/main/scala/co/s4n/duvanagudelo-calnatresta/Main.scala
@@ -0,0 +1,42 @@
+package co.s4n.calnat
+
+import scala.io.Stdin
+
+object Main extends App {
+  def leerInt(prompt:Sring):Int ={
+    val s = StdIn.readLine(prompt)
+    s.toInt
+  }
+}
+def esCero(nat:Nat) = nat match {
+  case Cero() => true
+  case Suc(Nat) => false
+}
+def esMayorIgual(nat1:Nat,nat2:Nat):Boolean = nat1 match {
+  case Cero() => nat2 match {
+    case Cero() => true
+    case _ => false
+  }
+  case Suc(pnat) => nat2 match {
+    case Cero() => true
+    case Suc(snat) => es esMayorIgual(pnat,snat)
+  }
+}
+def conIntANat(n:Int) = {
+  @scala.annotation.tailrec
+  def a(n:Int, acum:Nat):Nat = n match {
+    case 0 => acum
+    case n => a(n-1,Suc(acum))
+    }
+    a(n,Cero())
+    }
+
+def imprimirNat(n:Int):String
+  @scala.annotation.tailrec
+  def a(n:Int, acum:String):String = n match {
+    case 0 => acum
+    case n => a(n-1,"Suc("+(acum)+")")
+    }
+    a(n,"Cero")
+}
+def restaNat(nat1:Nat,nat2:Nat)=
